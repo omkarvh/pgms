582200a06d08d1c75ad730ce43c54fa4a6ee03eb
Omkar Hiremath <omkarvh56@gmail.com>
2026-06-04 19:10:13 +0530

    Updates

---
+++ assets/deleteRequests-C9M_hw8h.js
@@ -0,0 +1,1 @@
+import{a as e,b as t,u as n,v as r}from"./index-DX96c6Q1.js";async function i(i,a,o){await n(r(e,`deleteRequests`),{collectionName:i,docId:a,label:o,status:`pending`,createdAt:t()}),alert(`Delete request sent to admin for "${o}"`)}export{i as t};
